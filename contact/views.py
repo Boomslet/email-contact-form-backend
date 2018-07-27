2d2ecdf613d7867e55a2ccc67f46208297b5378a
Mark Boon <boomer_cant_be_stuffed@Hotmail.com>
2018-07-27 23:09:23 +0800

    added console email endpoint

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,6 @@
+from django import forms
 from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 
@@ -6,22 +8,22 @@ from .models import Contact
 from .serializers import ContactSerializer
 
 
-RECIPIENTS = []
+RECIPIENTS = ['m.boon93@hotmail.com']
 
 
 class ContactView(ModelViewSet):
     serializer_class = ContactSerializer
 
     def send_contact(self, request):
-        subject = request.data['subject']
-        message = request.data['message']
-        from_address = request.data['from_address']
-        
+
+        subject = request.POST.get('subject', '')
+        message = request.POST.get('message', '')
+        from_address = request.POST.get('from_address', '')
         if subject and message and from_address:
             try:
-                send_mail(subject, message, from_address, RECIPIENTS)
+                send_mail(subject, message, from_address,
+                          ['admin@example.com'])
             except BadHeaderError:
-                return Response('Invalid header found.')
+                return HttpResponse('Invalid header found.')
             return Response({})
-        
         return Response({})
--- email_backend/settings.py
+++ email_backend/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 # Application definition
 
